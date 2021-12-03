0c08a8ac4608088eb888b1f2a741dd4054a5c3d3
hoo <hoo0681@gachon.ac.kr>
2021-12-03 17:47:11 +0900

    pod name은 필수

--- dags/kubepodop.py
+++ dags/kubepodop.py
@@ -61,7 +61,7 @@ run = KubernetesPodOperator(
     #    env
     #],
     #image_pull_secrets=[k8s.V1LocalObjectReference('image_credential')],
-    #name="job",
+    name="job",
     is_delete_operator_pod=True,
     get_logs=True,
     resources=pod_resources,
