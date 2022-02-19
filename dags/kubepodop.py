f63d00007d66fab8a6b0ea9131a89edcf0de0574
hoo <hoo0681@gachon.ac.kr>
2022-02-19 22:11:54 +0900

    .

--- dags/kubepodop.py
+++ dags/kubepodop.py
@@ -58,7 +58,7 @@ run = KubernetesPodOperator(
     namespace='fed-play-ground',
     image='docker.io/hoo0681/airflowkubepodimage:0.1',
     #cmds=["python3"],
-    cmds=["/bin/sh","-c","until curl -fsl http://localhost:4191/ready; \
+    cmds=["/bin/sh","-c","apt-get install -y curl; until curl -fsl http://localhost:4191/ready; \
         do echo \"Waiting for Sidecar...\"; sleep 3; done; echo \"Sidecar available. Running the command...\"; \
         git clone -b ${GIT_TAG} ${REPO_URL} /app; \
         python3 -m pip install -r /app/requirements.txt; \
