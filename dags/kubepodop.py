e598bbe0e70668469807e87ae8839c394627b1b6
hoho <kumass2021@gmail.com>
2022-03-28 07:30:16 +0900

    Update kubepodop.py

--- dags/kubepodop.py
+++ dags/kubepodop.py
@@ -72,7 +72,7 @@ port = k8s.V1ContainerPort(container_port=8080)
 
 run = KubernetesPodOperator(
     task_id="kubernetespodoperator",
-    namespace='fed-repl-MJH',
+    namespace='fed-repl-mjh',
     image='docker.io/hoo0681/airflowkubepodimage:0.1',
     #cmds=["python3"],
     #cmds=["/bin/sh","-c","apt-get install -y curl; until curl -fsl http://localhost:4191/ready; \
