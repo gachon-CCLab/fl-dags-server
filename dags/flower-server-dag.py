a1ad05e88266357d83e48609a5b418d551fbb833
Semo <tpah20@gmail.com>
2022-08-01 19:21:26 +0900

    dag name 변경

--- dags/flower-server-dag.py
+++ dags/flower-server-dag.py
@@ -9,7 +9,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 
-dag_id = 'flower-server-NEWS-dag'
+dag_id = 'flower-server-dag'
 
 task_default_args = {
     'owner': 'airflow',
